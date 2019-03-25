0c439c7472f64604c27ffe53ce16ac2239833fea
John Ray <john@johnraymon.com>
2019-03-25 04:46:32 -0400

    require fields on event model

--- models/Event.js
+++ models/Event.js
@@ -1,16 +1,20 @@
 var mongoose = require("mongoose")
 
 var EventSchema = mongoose.Schema({
-  name: String,
+  name: { type: String, required: [ true, 'is required' ] },
   host: { type: mongoose.Schema.Types.ObjectId, ref: "Host" },
   currentListings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Listing"}],
-  date: Date,
-  startTime: Date,
-  endTime: Date,
+  date: { type: Date, required: [ true, 'is required'] },
+  startTime: { type: Date, required: [ true, 'is required' ] },
+  endTime: { type: Date, required: [ true, 'is required' ] },
   address: {
-    street: String,
-    city: String,
-    state: String,
-    zip: String
+    street: { type: String, required: [ true, 'is required' ] },
+    city: { type: String, required: [ true, 'is required' ] },
+    state: { type: String, required: [ true, 'is required' ] },
+    zip: { type: String, required: [ true, 'is required' ] }
   }
 })
+
+var Event = mongoose.model('Event', EventSchema)
+
+module.exports = Event
