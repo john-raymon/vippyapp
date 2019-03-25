671e69895af5f7474f041ed6e8175ce5b3752717
John Ray <john@johnraymon.com>
2019-03-25 03:46:10 -0400

    add stripe properties to config, and publicDomain

--- config/index.js
+++ config/index.js
@@ -1,3 +1,11 @@
 module.exports = {
-  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
+  stripe: {
+    secret_key: process.env.STRIPE_SECRET_KEY,
+    publishable_key: process.env.STRIPE_PUBLISHABLE_KEY,
+    client_id: process.env.NODE_ENV === 'production' ? process.env.STRIPE_CLIENT_ID : process.env.STRIPE_TEST_CLIENT_ID,
+    authorizeUri: 'https://connect.stripe.com/express/oauth/authorize',
+    tokenUri: 'https://connect.stripe.com/oauth/token'
+  },
+  public_domain: "localhost:3000"
 }
