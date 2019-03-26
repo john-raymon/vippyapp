8c6929e4998cec809d562043b454ec5827cbad2e
John Ray <john@johnraymon.com>
2019-03-25 21:00:34 -0400

    create endpoint to get all events for host

--- routes/beta_api/event.js
+++ routes/beta_api/event.js
@@ -1,29 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var querystring = require("querystring")
-var request = require("request")
-
+var querystring = require("querystring");
+var request = require("request");
 
 // auth middleware
-var auth = require('../authMiddleware')
-var { hostMiddleware } = require('./host')
+var auth = require("../authMiddleware");
+var { hostMiddleware } = require("./host");
 
 // models
-var Event = require("../../models/Event")
-var Host = require("../../models/Host")
+var Event = require("../../models/Event");
+var Host = require("../../models/Host");
 
 // config
-var config = require("./../../config")
+var config = require("./../../config");
 
-router.post('/', auth.required, hostMiddleware, function(req, res, next) {
-  console.log('the req.body at POST event/ endpoint is', req.body)
-  const { vippyHost:host } = req
-  if (!host) {
-    return res.status(401).json({ error: "You must be an Authenticated Host" })
-  }
+router.post("/", auth.required, hostMiddleware, function(req, res, next) {
+  console.log("the req.body at POST event/ endpoint is", req.body);
+  const { vippyHost: host } = req;
   const event = new Event({
     name: req.body.name,
-    host: host._id,
+    host: host,
     date: req.body.eventDate || req.body.date,
     startTime: req.body.startTime,
     endTime: req.body.endTime,
@@ -31,15 +27,28 @@ router.post('/', auth.required, hostMiddleware, function(req, res, next) {
       street: req.body.street,
       city: req.body.city,
       state: req.body.state,
-      zip: req.body.zip || req.body.zipcode,
+      zip: req.body.zip || req.body.zipcode
     }
-  })
+  });
 
-  return event.save()
-  .then((event) => {
-    res.json({ event })
-  })
-  .catch(next)
-})
+  return event
+    .save()
+    .then(savedEvent => {
+      res.json({ event: savedEvent.toJSONFor() });
+    })
+    .catch(next);
+});
 
-module.exports = router
+router.get("/", auth.required, hostMiddleware, function(req, res, next) {
+  const { vippyHost } = req;
+  Event.find({ host: vippyHost._id })
+    .populate("host")
+    .populate("currentListings")
+    .exec()
+    .then(events => {
+      res.json({ events: events.map((event, index) => event.toJSONFor()) });
+    })
+    .catch(next);
+});
+
+module.exports = router;
