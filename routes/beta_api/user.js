b7768cdc88f61ceb439ff41502abd05d7bd1e083
John Ray <john@johnraymon.com>
2019-03-28 19:04:44 -0400

    implement logic responsible for using a verification code to verify a phone number being used to register a user

--- routes/beta_api/user.js
+++ routes/beta_api/user.js
@@ -1,44 +1,122 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var request = require("request");
+var { parsePhoneNumber, ParseError } = require("libphonenumber-js");
 
 // models
-var User = require("../../models/User")
+var User = require("../../models/User");
 
 // passports
-var { userPassport } = require("./../../config/passport")
+var { userPassport } = require("./../../config/passport");
 
-router.post('/', function(req, res, next) {
-  console.log('the req is', req.body)
-  const user = new User({
-    email: req.body.email,
-    fullname: req.body.fullname,
-    zipcode: req.body.zipcode,
-    phonenumber: req.body.phonenumber
+router.post("/", function(req, res, next) {
+  const requiredProps = [
+    "email",
+    "fullname",
+    "zipcode",
+    "phonenumber",
+    "verification_code"
+  ];
+  let isBodyMissingProperty = false;
+
+  const propErrors = requiredProps.reduce((errors, prop) => {
+    if (!req.body[prop]) {
+      isBodyMissingProperty = true;
+      errors[prop] = `is required`;
+    }
+    return errors;
+  }, {});
+
+  if (isBodyMissingProperty) {
+    return res.status(400).json({ errors: propErrors });
+  }
+  // we have all the requiredProps so continue
+  const userNum = req.body.phonenumber;
+  // validate phonenumber
+  new Promise((resolve, reject) => {
+    try {
+      const parsedPhoneNumber = parsePhoneNumber(userNum, "US");
+      if (!parsedPhoneNumber.isValid()) {
+        return res.status(400).json({ error: "The phone number is not valid" });
+      }
+      resolve(parsedPhoneNumber);
+    } catch (error) {
+      if (error instanceof ParseError) {
+        return res
+          .status(400)
+          .json({ error: "The phone number may be invalid" });
+      }
+      reject(error);
+    }
   })
+    .then(parsedPhoneNumber => {
+      const nationalNumber = parsedPhoneNumber.nationalNumber;
+      const countryCallingCode = parsedPhoneNumber.countryCallingCode;
+      // now that phone number is still valid, we don't need to check if the number is unregistered because if they were able to initiate
+      // a twilio verification through the /twilio/send-onboard-code endpoint, then they were able to verify that the phone number
+      // is unregistered, so we don't have to worry about that at this point, we just need to attempt to verfiy with the code,
+      // if it succeeds then proceed to create the account for the user
+      return new Promise((resolve, reject) => {
+        request(
+          {
+            url:
+              "https://api.authy.com/protected/json/phones/verification/check",
+            method: "GET",
+            headers: {
+              "X-Authy-API-Key": process.env.TWILIO_ACCOUNT_SECURITY_API_KEY
+            },
+            qs: {
+              verification_code: req.body.verification_code,
+              phone_number: nationalNumber,
+              country_code: countryCallingCode
+            },
+            json: true
+          },
+          function(err, response, body) {
+            if (!body.success) {
+              console.log("the body is", body);
+              return res.status(400).json({ error: body.message });
+            }
+            // if verification was successful then resolve promise with twilio response body, and create the account
+            resolve(body);
+          }
+        );
+      });
+    })
+    .then(twilioRes => {
+      const user = new User({
+        email: req.body.email,
+        fullname: req.body.fullname,
+        zipcode: req.body.zipcode,
+        phonenumber: req.body.phonenumber
+      });
 
-  user.setPassword(req.body.password)
+      user.setPassword(req.body.password);
 
-  user.save().then(function() {
-    return res.json({ user: user.toAuthJSON()})
-  }).catch(next)
-})
+      return user.save().then(function() {
+        return res.json({ user: user.toAuthJSON() });
+      });
+    })
+    .catch(next);
+});
 
-router.post('/login', function(req, res, next) {
+router.post("/login", function(req, res, next) {
   if (!req.body.email || !req.body.password) {
-    res.status(422).json({errors: {"email and password": "are required to login"}})
+    res
+      .status(422)
+      .json({ errors: { "email and password": "are required to login" } });
   }
 
-  userPassport.authenticate('local', function(err, user, data) {
-
+  userPassport.authenticate("local", function(err, user, data) {
     if (err) {
-      return next(err)
+      return next(err);
     }
 
     if (!user) {
-      return res.status(422).json(data)
+      return res.status(422).json(data);
     }
-    return res.json({ user: user.toAuthJSON() })
-  })(req, res, next)
-})
+    return res.json({ user: user.toAuthJSON() });
+  })(req, res, next);
+});
 
-module.exports = router
+module.exports = router;
