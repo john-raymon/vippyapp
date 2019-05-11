7584943f467c8804b75ad9d919d772291f7bac81
John Ray <john@johnraymon.com>
2019-05-11 05:02:25 -0400

    update config/passport : implement passport for promoter resource, also configure Local Strategy to pass request object to callback, to allow querying by req.body.venueId

--- config/passport.js
+++ config/passport.js
@@ -1,38 +1,80 @@
 var Passport = require("passport").Passport,
-    userPassport = new Passport(),
-    hostPassport = new Passport()
-var LocalStategy = require("passport-local").Strategy
-var mongoose = require("mongoose")
-var User = require("../models/User")
-var Host = require("../models/Host")
+  userPassport = new Passport(),
+  hostPassport = new Passport(),
+  promoterPassport = new Passport();
+var LocalStategy = require("passport-local").Strategy;
+var mongoose = require("mongoose");
+var User = require("../models/User");
+var Host = require("../models/Host");
+var Promoter = require("../models/Promoter");
 
-userPassport.use(new LocalStategy({
-  usernameField: "email",
-  passwordField: "password"
-}, function(email, password, done) {
-  User.findOne({ email: email }).then(function(user) {
-    if (!user || !user.validPassword(password)) {
-      return done(null, false, { errors: {'email or password': 'is invalid'} })
+userPassport.use(
+  new LocalStategy(
+    {
+      usernameField: "email",
+      passwordField: "password"
+    },
+    function(email, password, done) {
+      User.findOne({ email: email })
+        .then(function(user) {
+          if (!user || !user.validPassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" }
+            });
+          }
+          return done(null, user);
+        })
+        .catch(done);
     }
-    return done(null, user)
-  }).catch(done)
-}))
+  )
+);
 
-hostPassport.use(new LocalStategy({
-  usernameField: "email",
-  passwordField: "password"
-}, function(email, password, done) {
-  console.log('called@!! here!! inside local !!', email, password)
-  Host.findOne({ email: email }).then(function(host) {
-    if (!host || !host.validPassword(password)) {
-      return done(null, false, { errors: {'email or password': 'is invalid'} })
+hostPassport.use(
+  new LocalStategy(
+    {
+      usernameField: "email",
+      passwordField: "password"
+    },
+    function(email, password, done) {
+      console.log("called@!! here!! inside local !!", email, password);
+      Host.findOne({ email: email })
+        .then(function(host) {
+          if (!host || !host.validPassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" }
+            });
+          }
+          return done(null, host);
+        })
+        .catch(done);
     }
-    return done(null, host)
-  }).catch(done)
-}))
+  )
+);
 
+promoterPassport.use(
+  new LocalStategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      passReqToCallback: true
+    },
+    function(req, username, password, done) {
+      Promoter.findOne({ username: username, venueId: req.body.venueId })
+        .then(function(promoter) {
+          if (!promoter || !promoter.validPassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" }
+            });
+          }
+          return done(null, promoter);
+        })
+        .catch(done);
+    }
+  )
+);
 
 module.exports = {
   hostPassport,
-  userPassport
-}
+  userPassport,
+  promoterPassport
+};
