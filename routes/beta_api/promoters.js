616d54fbdcbbb475c4aa1e84e7b62646340d4e71
John Ray <john@johnraymon.com>
2019-05-10 20:53:54 -0400

    create routers/beta_api/promoters : implement promoters express.Router, also implement Create endpoint at / path to be able to create a Promoter document if logged in as Host/Venue

---
+++ routes/beta_api/promoters.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+var querystring = require("querystring");
+var request = require("request");
+
+// auth middleware
+var auth = require("../authMiddleware");
+var { hostMiddleware } = require("./host");
+
+// models
+var Promoter = require("../../models/Promoter");
+var Host = require("../../models/Host");
+
+// config
+var config = require("./../../config");
+
+// utils
+var createId = require("./../../utils/createId");
+
+router.post("/", auth.required, hostMiddleware, function(req, res, next) {
+  const { vippyHost: host } = req;
+  const promoter = new Promoter({
+    username: req.body.username || createId(6),
+    venue: host,
+    venueId: host.venueId,
+    fullname: req.body.fullname
+  });
+
+  promoter.setPassword(req.body.password);
+
+  return promoter
+    .save()
+    .then(savedPromoter => {
+      res.json({ promoter: savedPromoter.getPromoter() });
+    })
+    .catch(next);
+});
+
+module.exports = router;
