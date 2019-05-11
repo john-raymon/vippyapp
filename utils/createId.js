d606ca294ca8e591f0f274fd8745f8a67552c443
John Ray <john@johnraymon.com>
2019-05-10 20:57:43 -0400

    update utils/createId : include comment stating stackoverflow source for utitlity function

--- utils/createId.js
+++ utils/createId.js
@@ -1,3 +1,6 @@
+// found on stackoverflow
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+
 function makeid(length) {
   var result = "";
   var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
