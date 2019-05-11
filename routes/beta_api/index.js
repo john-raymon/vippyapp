b68648c5ad241c12ab4b75fd9c6574b1ef3ddfa7
John Ray <john@johnraymon.com>
2019-05-10 20:55:10 -0400

    update routers/beta_api/index : use promoters Router  and mount on beta_api Router at "/promoters" path.

--- routes/beta_api/index.js
+++ routes/beta_api/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.use("/host", require("./host").router);
+router.use("/promoters", require("./promoters"));
 router.use("/user", require("./user"));
 router.use("/event", require("./event"));
 router.use("/listing", require("./listing"));
