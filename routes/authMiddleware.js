a645ce60d68f824659d1bd4efa6032429a909451
John Ray <john@johnraymon.com>
2019-05-12 21:47:36 -0400

    update routes/authMiddleware : populate venue field of vippyPromoter. Implement hostOrPromoterMiddleware , that takes in doNotAllowPromoter argument which is false by default. return hostOrPromoterOnlyMiddleware or hostOnlyMiddleware function depending on the doNotAllowPromoter argument, to prevent accessing of endpoint from non host or promoter accounts.

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -15,7 +15,6 @@ function getToken(req) {
   ) {
     return req.headers.authorization.split(" ")[1];
   }
-
   return null;
 }
 
@@ -62,6 +61,8 @@ var auth = {
         .catch(next);
     } else if (currentAuth.sub === "promoter") {
       Promoter.findById(currentAuth.id)
+        .populate("venue")
+        .exec()
         .then(function(promoter) {
           if (!promoter) {
             return res.status(401).json({
@@ -74,6 +75,97 @@ var auth = {
         })
         .catch(next);
     }
+  },
+  hostOrPromoterMiddleware: function(doNotAllowPromoter = false) {
+    if (doNotAllowPromoter) {
+      return function hostMiddleware(req, res, next) {
+        const hostAuth = req.auth;
+        if (hostAuth.sub !== "host") {
+          return next({ name: "UnauthorizedError" });
+        }
+        Host.findById(hostAuth.id)
+          .then(function(host) {
+            if (!host) {
+              return next({
+                name: "UnauthorizedError",
+                message: "You must be logged in as a Host"
+              });
+            }
+            req.vippyHost = host;
+            next();
+          })
+          .catch(next);
+      };
+    }
+    return function hostPromoterMiddleware(req, res, next) {
+      const hostAuth = req.auth;
+      if (hostAuth.sub !== "host" && hostAuth.sub !== "promoter") {
+        return next({
+          name: "UnauthorizedError",
+          message:
+            "You must be logged in as the venue host, or promoter of belonging to the venue host"
+        });
+      }
+      if (hostAuth.sub === "host") {
+        Host.findById(hostAuth.id)
+          .then(function(host) {
+            if (!host) {
+              return res
+                .status(401)
+                .json({ error: "You must be an Authenticated Host" });
+            }
+            req.vippyHost = host;
+            next();
+          })
+          .catch(next);
+      } else if (hostAuth.sub === "promoter") {
+        Promoter.findById(hostAuth.id)
+          .populate("venue")
+          .exec()
+          .then(function(promoter) {
+            if (!promoter) {
+              return next({
+                name: "UnauthorizedError",
+                message:
+                  "You must be logged in as a promoter belonging to the venue host"
+              });
+            }
+            req.vippyPromoter = promoter;
+            next();
+          })
+          .catch(next);
+      }
+    };
+  },
+  onlyPromoterWithCreateUpdateEventsPermissions: function(req, res, next) {
+    // this should be called after a hostOrPromoterMiddleware, or setUserOrHostPromoter middleware,since it depends on vippyPromoter to be on req object
+    // only allow vippyPromoter with truthy permissions.createUpdateEvents property to create Events for their Venue Host
+    if (
+      req.vippyPromoter &&
+      !req.vippyPromoter.permissions.createUpdateEvents
+    ) {
+      return next({
+        name: "UnauthorizedError",
+        message:
+          "You do not have proper permissions to create or update Events, contact your Venue Host"
+      });
+    }
+    return next();
+  },
+  onlyPromoterWithCreateUpdateListingsPermissions: function(req, res, next) {
+    // this should be called after a hostOrPromoterMiddleware, or setUserOrHostPromoter middleware,since it depends on vippyPromoter to be on req object
+    // only allow vippyPromoter with truthy permissions.createUpdateEvents property to create Events for their Venue Host
+    if (
+      req.vippyPromoter &&
+      !req.vippyPromoter.permissions.createUpdateListings
+    ) {
+      return next({
+        name: "UnauthorizedError",
+        message:
+          "You do not have proper permissions to create or update Listings, contact your Venue Host"
+      });
+    }
+    return next();
   }
 };
 
