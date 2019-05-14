d1af3f728b18a6eb519dc9cc30b3973a59a1fe15
John Ray <john@johnraymon.com>
2019-05-13 23:50:03 -0400

    update routes/beta_api/listing : import multer-cloudinary imageParser and cloudinary SDK. implement patch endpoint to update listing, with proper authentications, and permissions, allow for updating listing's images, by uploading or destroying given ids. Also update Create listing endpoint to utilize imageParser middleware to allow for uploading of listingImages after, validating fields and initially creating document, to prevent image uploads when validation not proper.

--- routes/beta_api/listing.js
+++ routes/beta_api/listing.js
@@ -3,10 +3,14 @@ var router = express.Router();
 var querystring = require("querystring");
 var request = require("request");
 
+// cloudinary
+var cloudinary = require("cloudinary");
+
 // auth middleware
 var auth = require("../authMiddleware");
 var hostOrPromoterOnlyMiddleware = auth.hostOrPromoterMiddleware(false);
 var hostOnlyMiddleware = auth.hostOrPromoterMiddleware(true);
+var imageParser = require("./../../config/multer-cloudinary");
 
 // models
 var Event = require("../../models/Event");
@@ -15,6 +19,9 @@ var Listing = require("../../models/Listing");
 
 // config
 var config = require("./../../config");
+
+// utils
+var isBodyMissingProps = require("./../../utils/isBodyMissingProps");
 
 router.param("listing", function(req, res, next, listingId) {
   Listing.findById(listingId)
@@ -25,7 +32,6 @@ router.param("listing", function(req, res, next, listingId) {
     })
     .exec()
     .then(function(listing) {
-      console.log("the listing is", listing);
       if (!listing) {
         return res.sendStatus(404);
       }
@@ -35,6 +41,102 @@ router.param("listing", function(req, res, next, listingId) {
     .catch(next);
 });
 
+router.patch(
+  "/:listing",
+  auth.required,
+  hostOrPromoterOnlyMiddleware,
+  auth.onlyPromoterWithCreateUpdateListingsPermissions,
+  imageParser.array("listingImages", 5),
+  async function(req, res, next) {
+    const { vippyHost, vippyPromoter, listing: vippyListing } = req;
+    console.log("got here");
+    // if host , check if event belongs to host/
+    // if promoter, check if event belongs to promoter's host
+    if (
+      (vippyHost && vippyHost.id !== vippyListing.host.id) ||
+      (vippyPromoter && vippyPromoter.venue.id !== vippyListing.host.id)
+    ) {
+      return next({
+        name: "UnauthorizedError",
+        message:
+          "You must the venue host of this event or promoter of the venue host of this event with proper permissions to make changes to this event"
+      });
+    }
+
+    const whitelistedKeys = [
+      "quantity",
+      "unlimitedQuantity",
+      "bookingDeadline"
+    ];
+    // changes to date, starttime, endtime, address are not allowed to be updated after creation of event,
+    // a deactivation of this event along with a new event with the
+    // preferred date, startTime, endtime, address will need to take be created
+    // /event/deactivate endpoint will be used for deactivating since sideeffects
+    // and constrains will need be to checked, deactivating connected listings, refunding reservations.
+    // multiple event cancellations will result in suspension of host , set event cap in env variables
+
+    for (let prop in req.body) {
+      if (whitelistedKeys.includes(prop)) {
+        vippyListing[prop] = req.body[prop];
+      }
+    }
+
+    let newImages = {};
+    if (req.files) {
+      newImages = req.files.reduce((newImagesObj, file) => {
+        newImagesObj[file.public_id] = {
+          url: file.url,
+          public_id: file.public_id
+        };
+        return newImagesObj;
+      }, {});
+      let listingImagesObject = {}; // convert Map to JS Object
+      for (let [key, value] of vippyListing.images) {
+        listingImagesObject[key] = value;
+      }
+      vippyListing.images = { ...listingImagesObject, ...newImages };
+    }
+
+    if (req.body.imageIdsToRemove) {
+      for (let publicId of req.body.imageIdsToRemove) {
+        console.log("we have images to remove gghh", publicId);
+
+        try {
+          const destroyImage = await new Promise((resolve, reject) => {
+            cloudinary.v2.uploader.destroy(publicId, {}, (error, result) => {
+              if (error) reject(error);
+              resolve(result);
+            });
+          });
+          if (destroyImage) {
+            let listingImagesObject = {}; // convert Map to JS Object
+            for (let [key, value] of vippyListing.images) {
+              listingImagesObject[key] = value;
+            }
+            vippyListing.images = {
+              ...listingImagesObject,
+              ...newImages,
+              [publicId]: undefined
+            };
+          }
+        } catch (err) {
+          next(err);
+        }
+      }
+    }
+
+    vippyListing
+      .save()
+      .then(listing => {
+        return res.json({
+          success: true,
+          listing: listing.toJSONForHost(vippyHost || vippyPromoter.venue)
+        });
+      })
+      .catch(next);
+  }
+);
+
 // Create Listings -- only the Venue Host on the Event and Promoter (with proper permissions) of the Venue Host on Event can Create Listings
 router.post(
   "/",
@@ -43,13 +145,31 @@ router.post(
   auth.onlyPromoterWithCreateUpdateListingsPermissions,
   function(req, res, next) {
     const { vippyHost: host, vippyPromoter } = req;
+    let requiredProps = [
+      "eventId",
+      "name",
+      "guestCount",
+      "bookingPrice",
+      "unlimitedQuantity",
+      "quantity"
+    ];
+    const { hasMissingProps, propErrors } = isBodyMissingProps(
+      requiredProps,
+      req.body
+    );
+    if (hasMissingProps) {
+      return next({
+        name: "ValidationError",
+        errors: propErrors
+      });
+    }
     Event.findById(req.body.eventId)
       .populate("host")
       .populate("currentListings")
       .exec()
       .then(function(event) {
         if (!event) {
-          res.status(404).json({
+          return res.status(404).json({
             success: false,
             errors: {
               event: {
@@ -75,7 +195,7 @@ router.post(
         const listing = new Listing({
           name: req.body.name,
           guestCount: req.body.guestCount,
-          payAndWait: req.body.hasPayAndWait,
+          payAndWait: req.body.hasPayAndWait, // not implemented yet in v1
           // images: req.body.endTime, // use multer and cloudinary to store images
           bookingPrice: req.body.bookingPrice,
           disclaimers:
@@ -94,7 +214,32 @@ router.post(
         return Promise.all([listing.save(), event.save()]);
       })
       .then(function([listing]) {
-        res.json({ listing: listing._toJSON() });
+        req.vippyListing = listing;
+        next();
+      })
+      .catch(next);
+  },
+  imageParser.array("listingImages", 10),
+  function(req, res, next) {
+    const { vippyListing: listing } = req;
+    if (req.files) {
+      const newImages = req.files.reduce((newImagesObj, file) => {
+        newImagesObj[file.public_id] = {
+          url: file.url,
+          public_id: file.public_id
+        };
+        return newImagesObj;
+      }, {});
+      let listingImagesObject = {}; // convert Map to JS Object
+      for (let [key, value] of vippyListing.images) {
+        listingImagesObject[key] = value;
+      }
+      listing.images = { ...listingImagesObject, ...newImages };
+    }
+    listing
+      .save()
+      .then(listing => {
+        return res.json({ listing: listing._toJSON() });
       })
       .catch(next);
   }
