822a8a293fe9766b836dbd7db2693c4bb8f60528
John Ray <john@johnraymon.com>
2019-05-16 16:01:19 -0400

    update app : add console.log for debugging

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.log("the error in error handler is", err);
   if (isProduction) {
     console.log(err.stack); // print stacktrace
   }
