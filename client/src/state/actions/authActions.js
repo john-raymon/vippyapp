12e0a5fa64ee65490ba8c6d4ae0fa3ea1e828916
John Ray <john@johnraymon.com>
2019-05-17 01:38:36 -0400

    create client/src/state/actions/authActions : implement thunk action creators to make request regarding authenticating, along with related actions to be dispatched.

---
+++ client/src/state/actions/authActions.js
@@ -0,0 +1,32 @@
+import localforage from "localforage";
+import Agent from "./../../utils/agent";
+
+const agent = new Agent();
+
+export const attemptLogin = (email, password) => dispatch => {
+  return dispatch({
+    type: "LOGIN",
+    payload: agent
+      .authLogin(email, password, "api/user/login")
+      .then(res => {
+        if (res.success) {
+          localforage.setItem("jwt", res.user.token);
+          return { ...res, user: { ...res.user, token: undefined } };
+        }
+        return Promise.reject(res);
+      })
+      .catch(error => {
+        agent.setToken(null);
+        localforage.setItem("jwt", "");
+        return error;
+      })
+  });
+};
+
+export const logout = () => dispatch => {
+  agent.setToken(null);
+  localforage.setItem("jwt", "");
+  return dispatch({
+    type: "LOGOUT"
+  });
+};
