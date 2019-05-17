0fa2669c069f47db169583389bb2a7d2cec07d9c
John Ray <john@johnraymon.com>
2019-05-17 01:42:09 -0400

    create client/src/components/App : implement App component, render Header, along with simple paths for Homepage and not found. Wrapped App with custom M-ui theme, using muithemeprovider. implement ProtectedRoute HOC for redirecting to login while perserving previous location.

---
+++ client/src/components/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route, Link, Redirect, Switch } from "react-router";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+
+// Route Components
+import Homepage from "./Homepage";
+
+// View Components
+import Header from "./Header";
+
+// Redux Actions
+import { logout } from "../state/actions/authActions";
+
+// Styles
+import "../styles/application.css";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffffff"
+    },
+    secondary: {
+      main: "#333333"
+    }
+  }
+});
+
+const NotFound = props => {
+  return <div>Not Found </div>;
+};
+
+const mapStateToProps = state => ({
+  isAuth: state.auth.isAuth
+});
+
+const ProtectedRoute = connect(
+  mapStateToProps,
+  null
+)(({ component: Component, isAuth, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuth ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+});
+
+class App extends Component {
+  render() {
+    const { auth } = this.props;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="bg-black ph4">
+          <Header auth={auth} logout={logout} />
+          <main className="mainContainer">
+            <Switch>
+              <Route path="/" exact component={Homepage} />
+              <Route path="/*" component={NotFound} />
+            </Switch>
+          </main>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default connect(
+  state => ({ auth: state.auth }),
+  { logout }
+)(App);
