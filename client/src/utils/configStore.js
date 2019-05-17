f4923156988be567ad8ec9061a205d1eda9d851b
John Ray <john@johnraymon.com>
2019-05-17 01:32:06 -0400

    create client/src/utils/configStore : implement configuration of Redux store with middlewares, and other libraries to enhance store, and redux functionalities in general

---
+++ client/src/utils/configStore.js
@@ -0,0 +1,46 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import createRootReducer from "../state/reducers";
+import localforage from "localforage";
+import { createBrowserHistory } from "history";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react-native
+import { createStateSyncMiddleware } from "redux-state-sync";
+
+// middlewares
+import thunkMiddleware from "redux-thunk";
+import promiseMiddleware from "redux-promise-middleware";
+import logger from "redux-logger";
+import { routerMiddleware } from "connected-react-router";
+
+export const history = createBrowserHistory();
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"] // only auth will be persisted
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  createRootReducer(history)
+);
+
+const configureStore = persistedState => {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const middlewares = [
+    thunkMiddleware,
+    logger,
+    promiseMiddleware,
+    routerMiddleware(history),
+    createStateSyncMiddleware({ blacklist: ["@@router/LOCATION_CHANGE"] })
+  ];
+  let store = createStore(
+    persistedReducer,
+    composeEnhancers(applyMiddleware(...middlewares))
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
+
+export default configureStore;
