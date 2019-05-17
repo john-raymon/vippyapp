03cea8422669a13afafa1699f322597b4df46aff
John Ray <john@johnraymon.com>
2019-05-17 01:39:54 -0400

    update client/src/index : update entrypoint index.js to nest App component within neccessary providers from diffeerent services.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ConnectedRouter } from "connected-react-router";
+import configureStore, { history } from "./utils/configStore";
+
+import App from "./components/App";
+
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const { store, persistor } = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
