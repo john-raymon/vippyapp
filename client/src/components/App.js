156c6c2d78455c792c31313bc26ee2e6ee69662d
John Ray <john@johnraymon.com>
2019-05-20 16:08:04 -0400

    update client/src/components/App : remove importing of Link not being used

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Route, Link, Redirect, Switch } from "react-router";
+import { Route, Redirect, Switch } from "react-router";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 
 // Route Components
