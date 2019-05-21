16e45b0887522454d6eed6f96ef8056984734ca0
John Ray <john@johnraymon.com>
2019-05-20 23:42:30 -0400

    create client/src/state/actions/queriedActions : implement module to house actions related to queried state in redux store.

---
+++ client/src/state/actions/queriedActions.js
@@ -0,0 +1,24 @@
+import localforage from "localforage";
+import Agent from "./../../utils/agent";
+
+const agent = new Agent();
+
+export const queryByZipCode = zipCode => dispatch => {
+  return dispatch({
+    type: "FETCH_EVENTS_BY_ZIP",
+    payload: agent
+      ._get(`api/listing?zip=${encodeURIComponent(zipCode)}`)
+      .then(res => {
+        if (res.success) {
+          return res;
+        }
+        return Promise.reject(res);
+      })
+      .catch(error => {
+        if (process.env.NODE_ENV === "development") {
+          console.log("there was an error in queryByZipCode action", error);
+        }
+        return error;
+      })
+  });
+};
