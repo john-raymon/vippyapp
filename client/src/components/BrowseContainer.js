b349293efb936beb26d172a3d83ce38b2f2fea76
John Ray <john@johnraymon.com>
2019-05-23 00:05:04 -0400

    update client/src/components/BrowseContainer: send props over to event card, and listing card, remove commented card mark up

--- client/src/components/BrowseContainer.js
+++ client/src/components/BrowseContainer.js
@@ -65,78 +65,22 @@ class BrowseContainer extends Component {
         );
       }
       return events.map((event, key) => {
-        return <EventCard key={key} eventDate={event.date} />;
+        return (
+          <EventCard
+            key={key}
+            eventTitle={event.name}
+            venueInitial={event.host.venueName[0]}
+            eventDate={event.date}
+            eventStartTime={event.startTime}
+            eventEndTime={event.endTime}
+            venueName={event.host.venueName}
+            venueStreetAddress={event.address.street}
+            venueCityZipCode={`${event.address.city},${event.address.state} ${
+              event.address.zip
+            }`}
+          />
+        );
       });
-      // return (
-      //   <div className="mv2 w-100 pb1 ph1">
-      //     <Card className="flex flex-column">
-      //       <CardHeader
-      //         className="bg-mid-gray"
-      //         avatar={
-      //           <Avatar aria-label="Recipe" className="">
-      //             R
-      //           </Avatar>
-      //         }
-      //         action={
-      //           <IconButton>
-      //             <MoreVertIcon />
-      //           </IconButton>
-      //         }
-      //         title="DJ KHALED AT TIER - Ladies in Free"
-      //         subheader="June 1, 2019"
-      //         titleTypographyProps={{
-      //           className: "f7-important white-important"
-      //         }}
-      //         subheaderTypographyProps={{
-      //           className: "f7-important white-wash-important"
-      //         }}
-      //       />
-      //       <CardContent
-      //         component="div"
-      //         className="bg-mid-gray padding-top-025-important"
-      //       >
-      //         <div className="flex flex-row flex-wrap flex-grow-0 mw6">
-      //           <div className="flex flex-column flex-grow-1 self-start mb2">
-      //             <div className="flex items-center flex-grow-1 justify-between ph1 mb1 white">
-      //               <p className="michroma f8 b tracked ttu">Start Time:</p>
-      //               <p className="michroma f8 ml1">8:00pm</p>
-      //             </div>
-      //             <div className="flex items-center flex-grow-1 justify-between ph1 mb1 white">
-      //               <p className="michroma f8 b tracked ttu">End Time:</p>
-      //               <p className="michroma f8 ml1">2:00am</p>
-      //             </div>
-      //           </div>
-      //           <div className="flex flex-column white flex-grow-1 self-end tr">
-      //             <p className="michroma f8 b tracked ttu mb1">
-      //               Night Club Name
-      //             </p>
-      //             <p className="michroma f8 b tracked ttu mb1">123 Street</p>
-      //             <p className="michroma f8 b tracked ttu mb1">
-      //               Orlando, Fl 32825
-      //             </p>
-      //           </div>
-      //         </div>
-      //       </CardContent>
-      //       <CardActionArea>
-      //         <CardMedia
-      //           className="aspect-ratio aspect-ratio--16x9"
-      //           image="https://res.cloudinary.com/vippy/image/upload/v1558318218/vippy-event-images/nightclub-image_l5lcmq.jpg"
-      //           title="Club Tier"
-      //         />
-      //       </CardActionArea>
-      //       <CardActions className="bg-dark-gray">
-      //         <Button
-      //           size="small"
-      //           color="primary"
-      //           fullWidth
-      //           className="michroma-important f8-important tracked"
-      //         >
-      //           view event's packages
-      //         </Button>
-      //       </CardActions>
-      //     </Card>
-      //   </div>
-      // );
     };
 
     const listingsTabContent = () => {
@@ -165,96 +109,23 @@ class BrowseContainer extends Component {
       }
 
       return listings.map((listing, key) => {
-        return <ListingCard key={key} />;
+        return (
+          <ListingCard
+            key={key}
+            bookingDeadline={listing.bookingDeadline}
+            packageTitle={listing.name}
+            eventStartTime={listing.event.startTime}
+            eventEndTime={listing.event.endTime}
+            venueName={listing.host.venueName}
+            guestCount={listing.guestCount}
+            price={listing.bookingPrice}
+            venueStreetAddress={listing.event.address.street}
+            venueCityZipCode={`${listing.event.address.city},${
+              listing.event.address.state
+            } ${listing.event.address.zip}`}
+          />
+        );
       });
-      // return (
-      //   <Fragment>
-      //     <div className="mv2 w-100 pb1 ph1">
-      //       <Card className="flex flex-column">
-      //         <CardHeader
-      //           className="bg-mid-gray"
-      //           action={
-      //             <IconButton>
-      //               <MoreVertIcon />
-      //             </IconButton>
-      //           }
-      //           title="Package - Pop Up"
-      //           subheader="at Club Tier on June 1, 2019"
-      //           titleTypographyProps={{
-      //             className: "f7-important white-important"
-      //           }}
-      //           subheaderTypographyProps={{
-      //             className: "f7-important white-wash-important"
-      //           }}
-      //         />
-      //         <CardContent
-      //           component="div"
-      //           className="padding-top-025-important bg-mid-gray"
-      //         >
-      //           <div className="flex flex-row flex-wrap flex-grow-0">
-      //             <div className="flex pv1 pr1">
-      //               <div className="flex items-center justify-between ph1 mb1 white">
-      //                 <p className="michroma f8 b tracked ttu">Guest Count:</p>
-      //                 <p className="michroma f8 ml1">8</p>
-      //               </div>
-      //             </div>
-      //             <div className="flex pv1 pr1">
-      //               <div className="flex items-center justify-between ph1 mb1 white">
-      //                 <p className="michroma f8 b tracked ttu">Price:</p>
-      //                 <p className="michroma f8 ml1">$400.00</p>
-      //               </div>
-      //             </div>
-      //             <p className="flex w-100 pa1 michroma f8 b tracked ttu light-gray mt1 mb2 bg-black-10">
-      //               this package's event info :
-      //             </p>
-      //             <div className="flex flex-column flex-grow-1 self-start mb2">
-      //               <div className="flex items-center flex-grow-1 justify-between ph1 mb1 white">
-      //                 <p className="michroma f8 b tracked ttu">Start Time:</p>
-      //                 <p className="michroma f8 ml1">8:00pm</p>
-      //               </div>
-      //               <div className="flex items-center flex-grow-1 justify-between ph1 mb1 white">
-      //                 <p className="michroma f8 b tracked ttu">End Time:</p>
-      //                 <p className="michroma f8 ml1">2:00am</p>
-      //               </div>
-      //             </div>
-      //             <div className="flex flex-column white flex-grow-1 self-end tr">
-      //               <p className="michroma f8 b tracked ttu mb1">
-      //                 Night Club Name
-      //               </p>
-      //               <p className="michroma f8 b tracked ttu mb1">123 Street</p>
-      //               <p className="michroma f8 b tracked ttu mb1">
-      //                 Orlando, Fl 32825
-      //               </p>
-      //             </div>
-      //             <p className="flex w-100 flex-row items-center michroma f8 b tracked ttu white pointer">
-      //               <span className="w0 pr1">
-      //                 <img src={eventIcon} width="100%" height="auto" />
-      //               </span>
-      //               view event
-      //             </p>
-      //           </div>
-      //         </CardContent>
-      //         <CardActionArea>
-      //           <CardMedia
-      //             className="w-100 h5"
-      //             image="https://res.cloudinary.com/vippy/image/upload/v1558377293/vippy-event-images/IMG_0920_svg4mn.jpg"
-      //             title="Club Tier"
-      //           />
-      //         </CardActionArea>
-      //         <CardActions className="bg-dark-gray">
-      //           <Button
-      //             size="small"
-      //             color="primary"
-      //             fullWidth
-      //             className="michroma-important f8-important tracked"
-      //           >
-      //             Reserve Now
-      //           </Button>
-      //         </CardActions>
-      //       </Card>
-      //     </div>
-      //   </Fragment>
-      // );
     };
 
     return (
