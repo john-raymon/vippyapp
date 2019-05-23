4540a99db955615c74b93527c5aa4bdcd235aece
John Ray <john@johnraymon.com>
2019-05-23 00:09:31 -0400

    update client/src/state/selectors/getEventsAndListings: remove maping of events, also remove formating, rather move formating that is done for UI reasons to the render method where the UI is being rendered. also filter against cancelled events, or passed events, same for listings

--- client/src/state/selectors/getEventsAndListings.js
+++ client/src/state/selectors/getEventsAndListings.js
@@ -1,6 +1,8 @@
 import { createSelector } from "reselect";
 import _get from "lodash/get";
-import { formatEventDate } from "./../../utils/dateFns";
+import isFuture from "date-fns/is_future";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
+import { formatEventDate, formatEventTimes } from "./../../utils/dateFns";
 
 export default createSelector(
   state => {
@@ -15,17 +17,33 @@ export default createSelector(
     };
   },
   state => {
-    const { listings, events } = state;
-    // only map if listings no longer null
-    const newEvents =
+    const { events, listings } = state;
+    const allEvents =
       events &&
-      events.map(event => ({
-        ...event,
-        date: formatEventDate(event.date)
-      }));
+      events.filter(event => {
+        return !event.cancelled && isFuture(new Date(event.endTime));
+      });
+    const eventsById = events
+      ? events.reduce(function(obj, event) {
+          obj[event.id] = event;
+          return obj;
+        }, {})
+      : {};
+
+    const allListings =
+      listings &&
+      listings.filter(
+        listing =>
+          !listing.cancelled &&
+          isFuture(new Date(listing.bookingDeadline)) &&
+          isFuture(new Date(listing.event.endTime))
+      );
+
     return {
       ...state,
-      events: newEvents
+      events: allEvents,
+      eventsById: eventsById,
+      listings: allListings
     };
   }
 );
