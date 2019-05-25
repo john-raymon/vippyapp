e0aa74fe925bdc9d498e7422dbd0a957152abaa0
John Ray <john@johnraymon.com>
2019-05-25 18:11:33 -0400

    update client/src/utils/configStore: import localforage, implement middleware to set authentication tokens upon dispatch of certain actions, even async actions using promise middleware, it will look for certain action.types, by a criteria of set or unset actions, if an action.type is dispatched then check if it is a set or unset action, if set, look for token to set on localforage, if unset action then unset token from localforage

--- client/src/utils/configStore.js
+++ client/src/utils/configStore.js
@@ -4,12 +4,36 @@ import { createBrowserHistory } from "history";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react-native
 import { createStateSyncMiddleware } from "redux-state-sync";
+import localforage from "localforage";
 
 // middlewares
 import thunkMiddleware from "redux-thunk";
 import promiseMiddleware from "redux-promise-middleware";
 import logger from "redux-logger";
 import { routerMiddleware } from "connected-react-router";
+
+const setTokenInStorageMiddleware = ({
+  setActions = [],
+  unsetActions = []
+}) => store => next => action => {
+  const debugMode = () => {
+    if (process.env.NODE_ENV === "development") {
+      console.log(`The current action is: ${action.type}`);
+      console.log(`setActions: ${setActions}, unsetActions: ${unsetActions}`);
+    }
+  };
+  if (setActions.includes(`${action.type}`)) {
+    debugMode();
+    localforage.setItem(
+      "jwt",
+      action.payload.user.token || action.payload.token || ""
+    );
+  } else if (unsetActions.includes(action.type)) {
+    debugMode();
+    localforage.setItem("jwt", "");
+  }
+  next(action);
+};
 
 export const history = createBrowserHistory();
 
@@ -32,6 +56,10 @@ const configureStore = persistedState => {
     logger,
     promiseMiddleware,
     routerMiddleware(history),
+    setTokenInStorageMiddleware({
+      setActions: ["USER_LOGIN", "USER_REGISTER"],
+      unsetActions: ["LOGOUT"]
+    }),
     createStateSyncMiddleware({ blacklist: ["@@router/LOCATION_CHANGE"] })
   ];
   let store = createStore(
