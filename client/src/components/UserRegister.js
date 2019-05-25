5d3b90061887810c121e84e7bd14555177a18b5d
John Ray <john@johnraymon.com>
2019-05-24 20:58:26 -0400

    create client/src/components/UserRegister: implement UserRegister component to render on "/sign-up" routes

---
+++ client/src/components/UserRegister.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class UserRegister extends Component {
+  render() {
+    return <div className="white">Sign Up</div>;
+  }
+}
+
+export default UserRegister;
