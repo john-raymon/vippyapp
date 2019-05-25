23836d46a4392d1729d7c589135a1f06a89436f3
John Ray <john@johnraymon.com>
2019-05-25 19:08:12 -0400

    update client/src/utils/agent: update Agent class methods to call _initToken on main request action methods to get jwt token from localforage, using async/await. This is deferred to Agent rather than handling in thunk action creator or from state using getState.

--- client/src/utils/agent.js
+++ client/src/utils/agent.js
@@ -1,13 +1,18 @@
 import superagentPromise from "superagent-promise";
 import _superagent from "superagent";
+import localforage from "localforage";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-class Agent {
+export class Agent {
   constructor(token = null, API_ROOT = "/") {
     this.token = token;
     this.API_ROOT = API_ROOT;
     this._tokenPlugin = this._tokenPlugin.bind(this);
+  }
+
+  async _initToken() {
+    this.token = await localforage.getItem("jwt");
   }
 
   _tokenPlugin(req) {
@@ -25,45 +30,58 @@ class Agent {
     return res.body;
   }
 
-  _del(url) {
+  async _del(url) {
+    await this._initToken();
     return superagent
       .del(`${this.API_ROOT}${url}`)
       .use(this._tokenPlugin)
       .then(this._responseBody);
   }
 
-  _get(url) {
+  async _get(url) {
+    await this._initToken();
+    console.log("the token is!! -->", this.token);
     return superagent
       .get(`${this.API_ROOT}${url}`)
       .use(this._tokenPlugin)
       .then(this._responseBody);
   }
 
-  _patch(url, body) {
+  async _patch(url, body) {
+    await this._initToken();
     return superagent
       .patch(`${this.API_ROOT}${url}`, body)
       .use(this._tokenPlugin)
       .then(this._responseBody);
   }
 
-  _post(url, body) {
+  async _post(url, body) {
+    await this._initToken();
     return superagent
       .post(`${this.API_ROOT}${url}`, body)
       .use(this._tokenPlugin)
       .then(this._responseBody);
   }
 
-  postImages(url, files, imagesProp) {
+  async postImages(url, files, imagesProp) {
+    await this._initToken();
     return superagent
       .post(`${this.API_ROOT}${url}`)
       .use(this._tokenPlugin)
       .attach(imagesProp, files)
       .then(this._responseBody);
   }
-
   authLogin(email, password, loginEndpoint) {
     return this._post(loginEndpoint, { email, password });
-  }
+  } // move to UserEndpointAgent
 }
 
-export default Agent;
+export class UserEndpointAgent extends Agent {
+  constructor(token = null, API_ROOT = "/") {
+    super(token, API_ROOT);
+  }
+
+  sendOnBoardCode(phoneNumber) {
+    return this._get(`api/phone/send-onboard-code?phonenumber=${phoneNumber}`);
+  }
+}
