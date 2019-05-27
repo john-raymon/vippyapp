d35a4960abf66676d906b8a6ca1de453589f531a
John Ray <john@johnraymon.com>
2019-05-26 21:56:30 -0400

    update client/src/state/reducers/auth: update auth state on successful user registration, also reset auth when a user registration is rejected. also remove ref error in 'initialState.null'

--- client/src/state/reducers/auth.js
+++ client/src/state/reducers/auth.js
@@ -5,11 +5,22 @@ const initialState = {
 
 export default (state = initialState, { type, payload = {} }) => {
   switch (type) {
+    case `USER_REGISTER_FULFILLED`:
+      return {
+        ...state,
+        isAuth: true,
+        user: {
+          ...payload.user,
+          token: undefined
+        }
+      };
+    case `USER_REGISTER_REJECTED`:
+      return { ...state, isAuth: initialState.isAuth, user: initialState.user };
     case `${"LOGIN"}_FULFILLED`:
       return { ...state, isAuth: true, user: payload.user };
     case "LOGOUT":
     case `${"LOGIN"}_REJECTED`:
-      return { ...state, isAuth: initialState.isAuth, user: initialState.null };
+      return { ...state, isAuth: initialState.isAuth, user: initialState.user };
     default:
       return state;
   }
