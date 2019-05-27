438cc5add74da91df0135472f69c6afac75b193a
John Ray <john@johnraymon.com>
2019-05-26 21:47:39 -0400

    update client/src/utils/regExp: update regexp, add zipcode, update phonenumber to allow for symbols

--- client/src/utils/regExp.js
+++ client/src/utils/regExp.js
@@ -1,4 +1,7 @@
-export const phoneNumber = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+export const phoneNumber = /^[\+\d]?(?:[\d-.\s()]*)$/;
+
 // password src: https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
 // Minimum eight characters, atleast one number:
 export const password = /^(?=.*?[a-zA-z])(?=.*?[0-9]).{8,}$/;
+
+export const zipCode = /^[0-9]{5}(?:-[0-9]{4})?$/;
