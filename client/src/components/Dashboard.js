e9f598c632ffad3d116cf5ee432d15fdd669351f
John Ray <john@johnraymon.com>
2019-05-29 19:24:10 -0400

    create client/src/components/Dashboard: implement dashboard component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="flex flex-column center mw8 pt4">
+        <h1 className="michroma tracked lh-title white ttc f3 f2-ns pr4 mb2 mw6">
+          Dashboard
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
