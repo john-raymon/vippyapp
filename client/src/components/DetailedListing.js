8b622c85943a19372bda76502260425c21a10f83
John Ray <john@johnraymon.com>
2019-07-02 22:11:24 -0400

    update client/src/components/DetailedListing: update render method to return different variants of the UI dependent on the error, listing state. implement mark-up to display listing, destruct image objects from listing.images entries array.

--- client/src/components/DetailedListing.js
+++ client/src/components/DetailedListing.js
@@ -5,58 +5,88 @@ import tableImage from "../images/table.png";
 class DetailedListing extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      listing: null,
+      error: null
+    };
   }
   componentDidMount() {
     // fetch listing data
+    this.props.userAgent
+      ._get(`api/listing/${this.props.match.params.listingId}`)
+      .then(resp => {
+        this.setState({
+          listing: resp.listing,
+          error: null
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: "Sorry, we can't load the listing right now."
+        });
+      });
   }
   render() {
-    return (
-      <div className="pt4 mw8 center">
-        <div className="flex flex-row items-end w-100">
-          <div className="w-third w-25-l mr2">
-            <img
-              src={tableImage}
-              width="100%"
-              alt="Hero Background"
-              className="pn1"
-            />
-          </div>
-          <div className="w-third w-25-l mr2">
-            <div
-              style={{
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "contain",
-                backgroundImage: `url(${``})`
-              }}
-              className="dim aspect-ratio aspect-ratio--1x1 bg-vippy-1"
-            />
-          </div>
-          <div className="w-third w-25-l mr2">
-            <div
-              style={{
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "contain",
-                backgroundImage: `url(${``})`
-              }}
-              className="dim aspect-ratio aspect-ratio--1x1 bg-vippy-1"
-            />
-          </div>
-          <div className="w-third w-25-l mr2">
-            <div
-              style={{
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "contain",
-                backgroundImage: `url(${``})`
-              }}
-              className="dim aspect-ratio aspect-ratio--1x1 bg-vippy-1"
-            />
+    const { error, listing } = this.state;
+    console.log(listing);
+    if (error) {
+      return <p className="red">{error}</p>;
+    }
+    if (listing) {
+      const [
+        [firstImageKey, firstImage = { url: "" }] = [],
+        [secondImageKey, secondImage = { url: "" }] = [],
+        [thirdImageKey, thirdImage = { url: "" }] = []
+      ] = Object.entries(listing.images);
+      return (
+        <div className="pt4 mw8 center white">
+          <div className="flex flex-row items-end w-100">
+            <div className="w-third w-25-l mr2">
+              <img
+                src={tableImage}
+                width="100%"
+                alt="Hero Background"
+                className="pn1"
+              />
+            </div>
+            <div className="w-third w-25-l mr2">
+              <div
+                style={{
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundImage: `url(${firstImage.url})`
+                }}
+                className="dim aspect-ratio aspect-ratio--1x1 bg-vippy-1"
+              />
+            </div>
+            <div className="w-third w-25-l mr2">
+              <div
+                style={{
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundImage: `url(${secondImage.url})`
+                }}
+                className="dim aspect-ratio aspect-ratio--1x1 bg-vippy-1"
+              />
+            </div>
+            <div className="w-third w-25-l mr2">
+              <div
+                style={{
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundImage: `url(${thirdImage.url})`
+                }}
+                className="dim aspect-ratio aspect-ratio--1x1 bg-vippy-1"
+              />
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    return <p>Loading</p>;
   }
 }
 
