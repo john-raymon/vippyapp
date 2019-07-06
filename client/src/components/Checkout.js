4b3c08fa7ff3784c35eb7cf240f514c243f9cbcf
John Ray <john@johnraymon.com>
2019-07-05 23:01:31 -0400

    create client/src/components/Checkout: implement Checkout component to handle continuation of checkout flow

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,82 @@
+import React, { Component, Fragment } from "react";
+import ListingLineItem from "./ListingLineItem";
+import { ReservationForm } from "./DetailedListing";
+import { Elements, StripeProvider } from "react-stripe-elements";
+
+export default class Checkout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listing: null,
+      error: null,
+      formError: null
+    };
+  }
+  componentDidMount() {
+    if (
+      this.props.location.state &&
+      this.props.location.state.continueCheckout
+    ) {
+      this.props.userAgent
+        ._get(`api/listing/${this.props.location.state.listingId}`)
+        .then(resp => {
+          this.setState({
+            listing: resp.listing,
+            stripeToken: JSON.parse(
+              this.props.location.state.stripeTokenObject
+            ),
+            error: null
+          });
+        })
+        .catch(error => {
+          this.setState({
+            error: "Sorry, we can't load the listing right now."
+          });
+        });
+    } else {
+      return this.props.history.push({
+        pathname: "/"
+      });
+    }
+  }
+  render() {
+    const { listing, formError } = this.state;
+    const { userAgent } = this.props;
+    if (listing) {
+      return (
+        <StripeProvider apiKey="pk_test_2Txz4BEB02STeZraf70NgKYh">
+          <div className="flex flex-column mw8 center">
+            <h1 className="michroma white tracked lh-copy mb4">Checkout</h1>
+            <div className="flex flex-column flex-row-l">
+              <div className="w-100 w-50-l">
+                {listing && (
+                  <ListingLineItem
+                    boxTitle="You're almost done! Read the details of your reservation below."
+                    listing={listing}
+                    userAgent={userAgent}
+                  />
+                )}
+              </div>
+              <div className="flex flex-column w-100 w-50-l">
+                <div className="ReservationForm__container flex flex-column justify-center flex-grow-1 bg-vippy-1 w-100 mv2 mh2-l mv0-l br2">
+                  <Elements>
+                    <ReservationForm
+                      formError={formError}
+                      guestCount={listing.guestCount}
+                      bookingPrice={listing.bookingPrice}
+                      disclaimers={listing.disclaimers}
+                      cardElementWrapperWidthClass="w-100"
+                      disclaimersWrapperWidthClass="w-100"
+                      cardElementParentContainerClasses="flex flex-column w-90"
+                    />
+                  </Elements>
+                </div>
+              </div>
+            </div>
+          </div>
+        </StripeProvider>
+      );
+    }
+    return <p className="white"> Loading Checkout ... </p>;
+  }
+}
