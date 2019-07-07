765b32b82ce134682ed838004afc4f6cb70177a4
John Ray <john@johnraymon.com>
2019-07-06 20:24:48 -0400

    create client/src/components/DetailedReservation: implement DetailedReservation component and export it

---
+++ client/src/components/DetailedReservation.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class DetailedReservation extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>Reservation will go here</div>;
+  }
+}
