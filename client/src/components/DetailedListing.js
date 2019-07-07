a20c068135320afaa9eb0645896ed780b8ab6510
John Ray <john@johnraymon.com>
2019-07-06 20:26:06 -0400

    update client/src/components/DetailedListing: remove ReservationForm from module, import it instead from own module

--- client/src/components/DetailedListing.js
+++ client/src/components/DetailedListing.js
@@ -1,12 +1,8 @@
 import React, { Component, Fragment } from "react";
-import {
-  CardElement,
-  Elements,
-  injectStripe,
-  StripeProvider
-} from "react-stripe-elements";
+import { Elements, StripeProvider } from "react-stripe-elements";
 import { EventCard } from "./Cards";
 import tableImage from "../images/table.png";
+import ReservationForm from "./ReservationForm";
 
 // <form className="flex flex-column items-center pv4 w-100">
 //   <div className="flex flex-column flex-row-ns w-90">
@@ -27,88 +23,6 @@ import tableImage from "../images/table.png";
 //     </div>
 //   </div>
 // </form>
-
-const createOptions = (fontSize, padding) => {
-  return {
-    style: {
-      base: {
-        fontSize,
-        color: "#ffffff",
-        letterSpacing: "0.02em",
-        fontFamily: "michromaregular, Source Code Pro, monospace",
-        "::placeholder": {
-          color: "#ffffff"
-        },
-        padding
-      },
-      invalid: {
-        color: "#9e2146"
-      }
-    }
-  };
-};
-
-class _ReservationForm extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const {
-      cardElementWrapperWidthClass = "w-100 w-50-ns",
-      disclaimersWrapperWidthClass = "w-100 w-50-ns",
-      cardElementParentContainerClasses = "flex flex-column flex-row-ns w-90"
-    } = this.props;
-    return (
-      <form
-        className="flex flex-column items-center pv4 w-100"
-        onSubmit={e => {
-          e.preventDefault();
-          this.props.handleSubmit(this.props.stripe);
-        }}
-      >
-        <div className={`${cardElementParentContainerClasses}`}>
-          <div
-            className={`flex flex-column ${cardElementWrapperWidthClass} justify-center pb3`}
-          >
-            <CardElement
-              onReady={el =>
-                setTimeout(() => {
-                  el.focus();
-                }, 2000)
-              }
-              {...createOptions("13px")}
-            />
-          </div>
-          <div
-            className={`flex flex-column ${disclaimersWrapperWidthClass} ph4-ns pt2`}
-          >
-            <p className="michroma f8 tracked lh-copy white-60 pb2 ttu tj">
-              <span className="db vippy-yellow lh-title pb1">disclaimers</span>
-              {`${this.props.disclaimers}`}
-            </p>
-          </div>
-        </div>
-
-        <p className="michroma f5 tracked lh-title white pv3 ph1 ttu tc">
-          {`Total: $${this.props.bookingPrice} / up to ${
-            this.props.guestCount
-          } guest`}
-        </p>
-
-        <button className="vippyButton mt3 mv3 mw1 center dim" type="submit">
-          <div className="vippyButton__innerColorBlock">
-            <div className="w-100 h-100 flex flex-column justify-center">
-              <p className="michroma f8 tracked-1 b ttu lh-extra white-90 center pb1">
-                purchase
-              </p>
-            </div>
-          </div>
-        </button>
-      </form>
-    );
-  }
-}
-export const ReservationForm = injectStripe(_ReservationForm);
 
 class DetailedListing extends Component {
   constructor(props) {
