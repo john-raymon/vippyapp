d74417af58314575208bd7ab74e99b8c1509d7fe
John Ray <john@johnraymon.com>
2019-07-06 22:37:17 -0400

    update client/src/components/DetailedReservation: in componentDidMount set thankYou state based on the thankYou query param, also fetch reservation object from server

--- client/src/components/DetailedReservation.js
+++ client/src/components/DetailedReservation.js
@@ -3,8 +3,38 @@ import React, { Component } from "react";
 export default class DetailedReservation extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      thankYou: false,
+      error: null,
+      reservation: null
+    };
+  }
+  componentDidMount() {
+    const {
+      location: { parsedSearch },
+      userAgent
+    } = this.props;
+    this.setState({
+      thankYou: !!parsedSearch.thankYou
+    });
+    // fetch reservation from server
+    this.props.userAgent
+      ._get(`api/reservation/${this.props.match.params.reservationId}`)
+      .then(resp => {
+        this.setState({
+          reservation: resp.reservation,
+          error: null
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: "Sorry, we can't load your reservation right now."
+        });
+      });
   }
   render() {
+    const { reservation } = this.state;
+    console.log("the reservation is", reservation);
     return <div>Reservation will go here</div>;
   }
 }
