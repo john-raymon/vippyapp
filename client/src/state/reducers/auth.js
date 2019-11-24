0576eb1f84d1fb56129814fd91d21c923f77cefc
John Ray <john@trychec.com>
2019-11-24 12:04:32 -0500

    add relevant venue initial state to initiate state object, create venueReducer to handle updating venue related state

--- client/src/state/reducers/auth.js
+++ client/src/state/reducers/auth.js
@@ -1,6 +1,24 @@
 const initialState = {
   isAuth: false,
+  isVenueAuth: false,
+  venue: null,
   user: null
+};
+
+const venueReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "VENUE_REGISTER_REJECTED":
+      return { ...state, ...initialState };
+    case "VENUE_REGISTER_FULFILLED":
+      return {
+        ...state,
+        ...initialState,
+        isVenueAuth: true,
+        venue: payload.venueHost
+      };
+    default:
+      return state;
+  }
 };
 
 export default (state = initialState, { type, payload = {} }) => {
@@ -14,14 +32,17 @@ export default (state = initialState, { type, payload = {} }) => {
           token: undefined
         }
       };
-    case `USER_REGISTER_REJECTED`:
+    case "USER_REGISTER_REJECTED":
       return { ...state, isAuth: initialState.isAuth, user: initialState.user };
-    case `${"USER_LOGIN"}_FULFILLED`:
+    case "USER_LOGIN_FULFILLED":
       return { ...state, isAuth: true, user: payload.user };
     case "USER_LOGOUT":
-    case `${"USER_LOGIN"}_REJECTED`:
+    case "USER_LOGIN_REJECTED":
       return { ...state, isAuth: initialState.isAuth, user: initialState.user };
     default:
-      return state;
+      return {
+        ...state,
+        ...venueReducer(state, { type, payload })
+      };
   }
 };
