a8cf5dfdd46a6fe6e73e16547f860225c20679d1
John Ray <john@trychec.com>
2019-11-29 23:52:10 -0500

    modify register thunk action creator, to rather be a function that when invoked will return appropriate redux thunk action creator for relevant auth user, reg user or venue, later on promoter

--- client/src/state/actions/authActions.js
+++ client/src/state/actions/authActions.js
@@ -3,22 +3,55 @@ import { Agent } from "./../../utils/agent";
 
 const agent = new Agent();
 
-export const register = (user, userAgent) => dispatch => {
-  const body = {
-    ...user,
-    verification_code: user.verificationCode,
-    fullname: user.fullName,
-    phonenumber: user.phoneNumber,
-    zipcode: user.zipCode
-  };
-  return dispatch({
-    type: "USER_REGISTER",
-    payload: userAgent.create(body).catch(error => {
-      // TODO: actually integrate Raven or something similar
-      console.log("RAVEN: error in authActions register thunk", error);
-      throw error;
-    })
-  });
+export const register = (type = "user") => {
+  // returns the proper dispatch function according to the user type
+
+  if (type === "venue") {
+    return (venue, venueAgent) => dispatch => {
+      const body = {
+        fullname: venue.fullName,
+        zipcode: venue.zipCode,
+        phonenumber: venue.phoneNumber,
+        email: venue.email,
+        venueName: venue.venueName,
+        password: venue.password,
+        accessCode: venue.accessCode
+      };
+      return dispatch({
+        type: "VENUE_REGISTER",
+        payload: venueAgent.create(body).catch(error => {
+          // TODO: actually integrate Raven or something similar
+          console.log(
+            "RAVEN: error in authActions register venue thunk",
+            error
+          );
+          throw error;
+        })
+      });
+    };
+  }
+  if (type === "user") {
+    return (user, userAgent) => dispatch => {
+      const body = {
+        // TODO: don't rely on spreading the properties over for the body, explicity assign them over, that way one can look at the
+        // dispatch action and see all the properties the request body will have. Rather than looking for the parameters given on the invocation.
+        ...user, // reason for spreading is because user is sent with property names such as
+        // user.verifcationCode that that endpoint expects as verification_code
+        verification_code: user.verificationCode,
+        fullname: user.fullName,
+        phonenumber: user.phoneNumber,
+        zipcode: user.zipCode
+      };
+      return dispatch({
+        type: "USER_REGISTER",
+        payload: userAgent.create(body).catch(error => {
+          // TODO: actually integrate Raven or something similar
+          console.log("RAVEN: error in authActions register user thunk", error);
+          throw error;
+        })
+      });
+    };
+  }
 };
 
 export const login = (userCredentials, userAgent) => dispatch => {
