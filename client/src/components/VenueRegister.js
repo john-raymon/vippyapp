79b34474a2dc602ce90691ea9830c6a0cc0cb0bb
John Ray <john@trychec.com>
2019-11-29 23:52:43 -0500

    modify copied UserRegister to be VenueRegister

--- client/src/components/VenueRegister.js
+++ client/src/components/VenueRegister.js
@@ -7,9 +7,6 @@ import {
   zipCode as zipCodeRegExp
 } from "./../utils/regExp";
 
-// Redux Actions
-import { register as registerDispatch } from "./../state/actions/authActions";
-
 // components
 import RegisterFormTextField from "./FormField";
 import ListingLineItem from "./ListingLineItem";
@@ -17,38 +14,31 @@ import ListingLineItem from "./ListingLineItem";
 // svgs
 import GlobeIcon from "../svgs/globe-icon-svg";
 
-/**
-   * 1. on first submit, save all form date, init request to twilio withonly phone number
-   * 2. if first step successful, set state hasInitVerif to true, display neccessary UI such different submit button,
-   *    showing field for verification code, also showing resend code button, if not successful display errors
-   * 3. on second submit, with verifcation code, make request the CREATE users endpoint
-   *    attempt to create user with verification code, display errors if not successful
-        if user wants to verify with another number they will have to click a change number button and have the
-        the ui change accordingly to init proper request on future submit
-   * 4. if second submit successful, then user will be registered and proper state will be updated
-   */
-class VenueRegister extends Component {
+export default class VenueRegister extends Component {
   constructor(props) {
     super(props);
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.validate = this.validate.bind(this);
     this.resetErrorsState = this.resetErrorsState.bind(this);
     this.handleFormChange = this.handleFormChange.bind(this);
-    this.sendOnBoardCode = this.sendOnBoardCode.bind(this);
-    this.verifyAndCreateUser = this.verifyAndCreateUser.bind(this);
-    this.revertInitOfVerif = this.revertInitOfVerif.bind(this);
     this.state = {
-      hasInitVerif: false,
-      email: "",
-      phoneNumber: "",
-      password: "",
-      confirmPassword: "",
-      fullName: "",
-      verificationCode: "",
-      error: ""
+      newVenue: {
+        fullName: "",
+        zipCode: "",
+        phoneNumber: "",
+        email: "",
+        venueName: "",
+        password: "",
+        accessCode: "",
+        legalVenueName: "",
+        confirmPassword: "" // keep outside of newVenue since we're only concerned with this on client-side not sending it server
+      },
+      errors: {}
     };
   }
 
+  // on initial mount, we locationState from location prop using history to redirect based off the isAuth prop
+  // this will redirect any logged venue owner.
   componentDidMount() {
     const { isAuth, location, history } = this.props;
     const { state: locationState } = location;
@@ -61,6 +51,8 @@ class VenueRegister extends Component {
     }
   }
 
+  // we do the same thing we do in the initial mount, except if the isAuth prop changes
+  // it will trigger the componentDidUpdate lifecycle method thus causing a redirect or not
   componentDidUpdate() {
     const { isAuth, location, history } = this.props;
     const { state: locationState } = location;
@@ -73,46 +65,30 @@ class VenueRegister extends Component {
     }
   }
 
+  // resets error state
   resetErrorsState() {
-    const allErrors = Object.keys(this.state).reduce((allErrors, key) => {
-      const allKeyChars = key.toLowerCase();
-      if (allKeyChars.slice(-5) === "error") {
-        allErrors[key] = undefined;
-      }
-      return allErrors;
-    }, {});
     this.setState({
       error: "",
-      ...allErrors
+      errors: {}
     });
   }
 
-  verifyAndCreateUser(user) {
-    return this.props.userAgent.create({
-      ...user,
-      verification_code: user.verificationCode,
-      fullname: user.fullName,
-      phonenumber: user.phoneNumber
-    });
-  }
-
-  sendOnBoardCode(phoneNumber, email) {
-    return this.props.userAgent.sendOnBoardCode(phoneNumber, email);
-  }
-
-  validate(user, continueRegistration) {
-    const userSchema = yup.object().shape({
+  // validate the new venue object using Yup; Object Schema Validation
+  validate(venue) {
+    const venueSchema = yup.object().shape({
       email: yup
         .string()
         .email("Please enter a valid email")
-        .required("Your email is required"),
+        .required(
+          "The email of the venue or a representative of the venue is required"
+        ),
       phoneNumber: yup
         .string()
         .matches(phoneNumberRegExp, {
           message: "Your phone must be a valid phone number"
         })
         .label("Phone number")
-        .required("You must provide a phone number"),
+        .required("The venue's phone number is required."),
       password: yup
         .string()
         .min("8", "Password is too short - should be 8 chars minimum")
@@ -124,127 +100,103 @@ class VenueRegister extends Component {
       confirmPassword: yup
         .string()
         .oneOf([yup.ref("password"), null], "Passwords don't match")
-        .required("You must confirm the password"),
-      fullName: yup.string().required("Your full name is required"),
+        .required("Confirm your password"),
+      fullName: yup.string().required("The full name of an owner or manager."),
+      legalVenueName: yup
+        .string()
+        .required("The legal name of the business, or entity. (Example LLC)"),
       zipCode: yup
         .string()
         .matches(zipCodeRegExp, {
           message: "Please enter a valid zip code"
         })
-        .required("Please enter a zip code"),
-      verificationCode: yup.string().when("$continueRegistration", {
-        is: true,
-        then: yup.string().required("Please enter your verification code"),
-        otherwise: yup.string()
-      })
+        .required("Please enter the zip code of the venue."),
+      accessCode: yup
+        .string()
+        .required(
+          "Your access code is required to register. Contact us at info@getvippy.com if you forgot your code."
+        )
     });
-    return userSchema.validate(user, {
-      abortEarly: false,
-      context: { continueRegistration }
+    return venueSchema.validate(venue, {
+      abortEarly: false
     });
   }
 
+  // handle any type of input type and update respective state property's value
   handleFormChange(e) {
     this.setState({
-      [e.target.name]: e.target.value
+      newVenue: {
+        ...this.state.newVenue,
+        [e.target.name]: e.target.value
+      }
     });
   }
 
-  revertInitOfVerif() {
-    this.setState({
-      ...this.state,
-      hasInitVerif: false
-    });
-  }
-
-  onFormSubmit(e, continueRegistration = false) {
+  // on a form submit, create object representing newVenue to be passed to this.validate method
+  // to be validated against our Yup schema above (see instance validate method)
+  // if validation successful then initiate request to Create endpoint for Venue via the
+  // venueRegisterDispatch binded ActionCreator method
+  // if any errors, state will be updated locally to reflect
+  // if successful then redux will be updated, thus updating this.props.isAuth
+  // causing redirection within componentDidUpdate
+  onFormSubmit(e) {
     e.preventDefault();
     this.resetErrorsState();
-    const newUser = {
-      email: this.state.email,
-      phoneNumber: this.state.phoneNumber,
-      password: this.state.password,
-      confirmPassword: this.state.confirmPassword,
-      fullName: this.state.fullName,
-      zipCode: this.state.zipCode.trim(),
-      verificationCode: this.state.verificationCode || ""
+    // construct new venue object that will be passed to yup for schema validation
+    const newVenue = {
+      ...this.state.newVenue
     };
-    this.validate(newUser, continueRegistration)
-      .then(validatedNewUser => {
-        this.setState({
-          ...this.state,
-          prevValidatedUser: validatedNewUser
-        });
-        if (continueRegistration) {
-          return this.props
-            .registerDispatch(validatedNewUser, this.props.userAgent)
-            .then(() => {
-              this.props.setSnackbar(
-                "Yay, your account was created successfully!"
-              );
-            });
-        }
-        return this.sendOnBoardCode(
-          validatedNewUser.phoneNumber,
-          validatedNewUser.email
-        ).then(res => {
-          if (res.success) {
-            window.scrollTo(0, 0);
-            return this.setState({
-              ...this.state,
-              hasInitVerif: true
-            });
-          } else {
-            return Promise.reject({
-              message:
-                res.error ||
-                "We are experiencing issues while attempting to send your number a verification. Please try a different number or try again later."
-            });
-          }
-        });
+    this.validate(newVenue)
+      .then(validatedNewVenue => {
+        return this.props.venueRegisterDispatch(
+          validatedNewVenue,
+          this.props.venueAgent
+        ); // Redux will update auth state and trigger a location change if successful login
       })
       .catch(error => {
-        // catch validation errors, and map them path/state name
+        // COMMON GOTCHA: The yup returns a ValidationError that has aggregated Yup ValidationError(s) within it
+        // our backend can/may return mongoose ValidationError(s) that do not contain an inner property but do
+        // contain an errors property
+
+        // catch validation errors
         if (error.name === "ValidationError") {
-          let allOtherErrors = {};
-          let errorsByPath = {};
-          if (!error.inner) {
-            allOtherErrors = Object.keys(error.errors).reduce(
-              (allOtherErrors, key) => {
-                allOtherErrors[`${key}Error`] = error.errors[key];
-                return allOtherErrors;
-              },
-              {}
-            );
-          } else {
-            errorsByPath = error.inner.reduce((errorsByPath, error) => {
-              errorsByPath[`${error.path}Error`] = error.message;
-              return errorsByPath;
-            }, {});
+          if (error.inner.length) {
+            // if has inner then it is a Yup ValidationError thrown from this.validate method
+            let errors = {};
+            error.inner.forEach(error => {
+              errors = {
+                ...errors,
+                [error.path]: error.message
+              };
+            });
+            return this.setState({
+              errors
+            });
+            // TODO: loop through each validationError within error.inner and create a property within this.state.errors using it's error.inner[0].path as the key
           }
-          return this.setState({
-            ...this.state,
-            ...allOtherErrors,
-            ...errorsByPath
-          });
+          if (error.errors.length) {
+            return this.setState({
+              errors: error.errors
+            });
+          }
+          // if errors is empty and inner doesn't exist then (since inner will never be empty if Yup validationError)
+          // state update below will be returned
         }
+        // any other error
         return this.setState({
-          error: error.message || error.error
+          error:
+            error.message ||
+            "We're sorry, we're experiencing technical issues. Please try again or contact us at info@getvippy.com."
         });
       });
   }
+
   render() {
-    const {
-      error,
-      hasInitVerif,
-      prevValidatedUser = {
-        fullName: "Nova Rae"
-      }
-    } = this.state;
+    const { error, errors } = this.state;
     return (
       <div className="flex flex-column flex-row-l mw8 center pv4 ph1">
         <div className="registerComponent flex flex-column w-100">
-          <h1 className="michroma tracked lh-title white tw-text-2xl lg:tw-text-4xl f2-ns tw-text-center mb2 mt0">
+          <h1 className="michroma tracked lh-title white tw-text-xl lg:tw-text-4xl f2-ns tw-text-center mb2 mt0">
             Optimize the reservation experience for your events. Register your
             venue.
           </h1>
@@ -255,23 +207,23 @@ class VenueRegister extends Component {
             </p>
           )}
 
-          <div className="tw-flex tw-flex-col md:tw-flex-row tw-py-6">
+          <div className="tw-flex tw-flex-col md:tw-flex-row tw-py-8">
             <form
               className="registerComponent__form flex flex-column tw-w-full md:tw-w-6/12 mt2"
               onChange={this.handleFormChange}
             >
               <div className="mb3 w-100">
                 <RegisterFormTextField
-                  placeholder="What's your email address?"
+                  placeholder="What's the venue's email?"
                   type="email"
                   label="Email"
                   name="email"
                   value={this.state.email}
                 />
 
-                {this.state.emailError && (
+                {errors.email && (
                   <p className="michroma f7 red o-60 pt1 tracked lh-copy">
-                    {this.state.emailError}
+                    {errors.email}
                   </p>
                 )}
               </div>
@@ -282,25 +234,12 @@ class VenueRegister extends Component {
                   type="text"
                   label="Phone Number"
                   name="phoneNumber"
-                  className={`${hasInitVerif ? "o-30" : ""}`}
                   value={this.state.phoneNumber}
-                  disabled={hasInitVerif}
                 />
 
-                {hasInitVerif && (
-                  <div className="flex flex-row items-center pt2">
-                    <button
-                      className="michroma outline-0 bn bg-transparent f8 white-90 o-60 pa0 tracked lh-title pointer"
-                      onClick={this.revertInitOfVerif}
-                    >
-                      verify a different number
-                    </button>
-                  </div>
-                )}
-
-                {this.state.phoneNumberError && (
+                {errors.phoneNumber && (
                   <p className="michroma f7 red o-60 pt1 tracked lh-copy">
-                    {this.state.phoneNumberError}
+                    {errors.phoneNumber}
                   </p>
                 )}
               </div>
@@ -312,9 +251,9 @@ class VenueRegister extends Component {
                   name="zipCode"
                   value={this.state.zipCode}
                 />
-                {this.state.zipCodeError && (
+                {errors.zipCode && (
                   <p className="michroma f7 red o-60 pt1 tracked lh-copy">
-                    {this.state.zipCodeError}
+                    {errors.zipCode}
                   </p>
                 )}
               </div>
@@ -326,9 +265,9 @@ class VenueRegister extends Component {
                   name="password"
                   value={this.state.password}
                 />
-                {this.state.passwordError && (
+                {errors.password && (
                   <p className="michroma f7 red o-60 pt1 tracked lh-copy">
-                    {this.state.passwordError}
+                    {errors.password}
                   </p>
                 )}
               </div>
@@ -340,9 +279,9 @@ class VenueRegister extends Component {
                   name="confirmPassword"
                   value={this.state.confirmPassword}
                 />
-                {this.state.confirmPasswordError && (
+                {errors.confirmPassword && (
                   <p className="michroma f7 red o-60 pt1 tracked lh-copy">
-                    {this.state.confirmPasswordError}
+                    {errors.confirmPassword}
                   </p>
                 )}
               </div>
@@ -354,39 +293,37 @@ class VenueRegister extends Component {
                   name="fullName"
                   value={this.state.fullName}
                 />
-                {this.state.fullNameError && (
+                {errors.fullName && (
                   <p className="michroma f7 red o-60 pt1 tracked lh-copy">
-                    {this.state.fullNameError}
+                    {errors.fullName}
                   </p>
                 )}
               </div>
-              {!hasInitVerif && (
-                <button
-                  className="vippyButton mt4 mw1 self-end dim"
-                  onClick={this.onFormSubmit}
-                  type="submit"
-                >
-                  <div className="vippyButton__innerColorBlock">
-                    <div className="w-100 h-100 flex flex-column justify-center">
-                      <p className="michroma f8 tracked-1 b ttu lh-extra white-90 center pb1">
-                        sign up
-                      </p>
-                    </div>
+              <button
+                className="vippyButton mt4 mw1 self-end dim"
+                onClick={this.onFormSubmit}
+                type="submit"
+              >
+                <div className="vippyButton__innerColorBlock">
+                  <div className="w-100 h-100 flex flex-column justify-center">
+                    <p className="michroma tracked-1 b ttu lh-extra white-90 center pb1">
+                      sign up
+                    </p>
                   </div>
-                </button>
-              )}
+                </div>
+              </button>
             </form>
-            <div className="tw-flex tw-justify-end tw-items-center tw-flex-grow tw-px-6 tw-order-first md:tw-order-none">
-              <div className="tw-flex tw-flex-col tw-w-full tw-items-end">
-                <div className="tw-flex tw-justify-between tw-items-center tw-max-w-sm">
-                  <div class="tw-w-10 tw-max-w-xs">
+            <div className="tw-flex tw-justify-end tw-items-center tw-flex-grow md:tw-px-6 tw-order-first md:tw-order-none">
+              <div className="tw-flex tw-flex-col tw-w-full md:tw-items-end">
+                <div className="tw-flex md:tw-justify-between tw-items-center tw-max-w-sm">
+                  <div className="tw-w-10 tw-max-w-xs tw-order-last md:tw-order-first">
                     <GlobeIcon />
                   </div>
-                  <p className="tw-w-48 tw-text-sm tw-text-right tw-text-white tw-leading-relaxed tw-tracking-wider">
+                  <p className="tw-w-48 tw-text-sm md:tw-text-right tw-text-white tw-leading-relaxed tw-tracking-wider">
                     We don’t charge anything up-front
                   </p>
                 </div>
-                <p className="tw-w-full tw-text-xs tw-text-gray-600 tw-mt-2 tw-text-right tw-leading-relaxed md:tw-max-w-sm lg:tw-max-md">
+                <p className="tw-w-full tw-text-xs tw-text-gray-600 tw-mt-2 md:tw-text-right tw-leading-relaxed md:tw-max-w-sm lg:tw-max-md">
                   We only charge a 15 percent fee when a reservation is made at
                   your venue. Allowing you to get up and running online with no
                   hassle.
@@ -399,8 +336,3 @@ class VenueRegister extends Component {
     );
   }
 }
-
-export default connect(
-  null,
-  { registerDispatch }
-)(VenueRegister);
